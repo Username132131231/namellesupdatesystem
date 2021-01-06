8822ecc499fb27f3216e28f5688376efd46b3411
Username132131231 <72160069+Username132131231@users.noreply.github.com>
2021-01-06 10:32:52 +0300

    Update last-link-zeusx.php

--- last-link-zeusx.php
+++ last-link-zeusx.php
@@ -1,1 +1,1 @@
-https://cdn.discordapp.com/attachments/795690030144290826/795974900355432448/Namelles_2.2_Krnl_Version.rar
+https://github.com/Username132131231/namellesupdatesystem/raw/main/Namelless%202.2.rar
