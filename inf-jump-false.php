c2b558b09a1b379ddc439d6a8a3f8d83c78e9a75
Username132131231 <72160069+Username132131231@users.noreply.github.com>
2021-01-10 22:50:14 +0300

    Update and rename inf-jump-false.php to 2.php

--- inf-jump-false.php
+++
@@ -1,6 +0,0 @@
-InfiniteJumpEnabled = false
-game:GetService("UserInputService").JumpRequest:connect(function()
-	if InfiniteJumpEnabled then
-		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-	end
-end)
